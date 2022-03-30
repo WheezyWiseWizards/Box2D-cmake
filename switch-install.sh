12aea5f50bdd512098fa21db3f1bb20edc02f125
Alexander Winter <a.w1nter@hotmail.com>
2022-03-29 22:32:30 -0400

    Switch compilation now compatible

---
+++ switch-install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -eu
+cmake -B build-switch -S . -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/DevkitA64Libnx.cmake
+cd build-switch
+make
+cd ..
+
+sudo cp build-switch/libBox2D-cmake.a $DEVKITPRO/portlibs/switch/lib/libBox2D.a
+sudo cp -r Box2D/Box2D/* $DEVKITPRO/portlibs/switch/include/
